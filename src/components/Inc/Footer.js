6bacf6684392e3799178daa2b25a32283a1c7cfd
intelhostbd <30995970+intelhostbd@users.noreply.github.com>
2021-07-17 09:38:44 -0700

    Update Footer.js

--- src/components/Inc/Footer.js
+++ src/components/Inc/Footer.js
@@ -4,7 +4,7 @@ import '../../styles/footer.css';
 export default function Footer() {
     return (
         <footer >
-            WinCash.com - &copy; 2021 to 2030 all right reserved.
+            WinCash - &copy; 2021 all right reserved.
         </footer>
     )
 }
